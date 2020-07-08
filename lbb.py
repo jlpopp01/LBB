e009aff4ffad71f37fd595e03f316ef9cce303ac
Jaye Popplewell <jlpopp01@gmail.com>
2020-07-08 13:01:53 -0400

    Start of something new

---
+++ lbb.py
@@ -0,0 +1,25 @@
+lbb = []
+
+entry = input("Would you like to make an entry? Y/N format. N to Quit. ")
+
+date = input("What date would you like to save? Please use MM/DD/YYYY format. ")
+note = input('What note would you like to save? Keep it short and sweet, 250 character limit.  ')
+
+if entry == "N"
+    break
+    else if input is != MM/DD/YYY
+    then print("Wrong format, please try again")
+    else if length(note) > 250 characters
+    then print("Keep it short and cover the topic")
+
+
+.join(date,note)
+
+fd = open(filename, "lbb.csv")
+input = raw_input(date + note)
+fd.write(input)
+
+.append(date + note) -> lbb.csv
+
+
+print (lbb) 
