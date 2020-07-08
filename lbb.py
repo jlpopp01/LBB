eee192a4601bfef06b6471916832024d247d51d8
Jaye Popplewell <jlpopp01@gmail.com>
2020-07-08 13:09:42 -0400

    Added comments to help flesh out ideas

--- lbb.py
+++ lbb.py
@@ -1,10 +1,15 @@
 lbb = []
 
+#Gather info from user I think this might need to be in the while loop
 entry = input("Would you like to make an entry? Y/N format. N to Quit. ")
-
 date = input("What date would you like to save? Please use MM/DD/YYYY format. ")
 note = input('What note would you like to save? Keep it short and sweet, 250 character limit.  ')
 
+#run loop until quit
+while entry = Y:
+    continue
+
+# check to make sure the entry is valid per perimeters
 if entry == "N"
     break
     else if input is != MM/DD/YYY
@@ -12,14 +17,16 @@ if entry == "N"
     else if length(note) > 250 characters
     then print("Keep it short and cover the topic")
 
-
+#merge input into one object
 .join(date,note)
 
+#write info to csv file
 fd = open(filename, "lbb.csv")
 input = raw_input(date + note)
 fd.write(input)
 
+#or append to database
 .append(date + note) -> lbb.csv
 
-
+#print entries
 print (lbb) 
