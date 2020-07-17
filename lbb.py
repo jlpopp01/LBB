fc1077e6ebe0c6d4910f886d231e4d69143cd0ac
Jaye Popplewell <jlpopp01@gmail.com>
2020-07-17 10:37:26 -0400

    inputs work, exceptions are there but it still isn't talking to itself

--- lbb.py
+++ lbb.py
@@ -1,20 +1,22 @@
 import datetime
+import re
 
 #check to see if user would like to make an entry
-
-date = input("What date would you like to save? Please use MM-DD-YYYY format. ")
-
 
 def lbb():
     entry = []
     while True:
         entry = input("Would you like to make an entry? Y/N format. N to Quit. ").lower()
         if entry == 'y':
-            make_note()
+            continue
         elif entry == 'n':
             break 
 
 #lbb() #activate
+
+lbb()
+
+date = input("What date would you like to save? Please use MM-DD-YYYY format. ")
 
 
 #this function checks to see if the date is valid
@@ -31,20 +33,10 @@ is_valid_date(date)
 note = input('What note would you like to save? Keep it short and sweet, 250 character limit.  ')
 
 def is_note_short(note):
-    if len(note) > 250:
+    if len(note) > 280:
         print("Twitter rules here, keep it short.")
 
 
 is_note_short(note)
-#merge input into one object
-#.join(date,note)
-
-#write info to csv file
-#fd = open(filename, "lbb.csv")
-#input = raw_input(date + " , " + note)
-#fd.write(input)
-
-#or append to database
-#.append(date + note) -> lbb.csv
 
 print(date + " " + note)
